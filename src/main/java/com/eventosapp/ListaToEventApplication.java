33851492eb32ccd399047795237b5d4cd7526f77
Wander Souza <wanderrodrigues_@outlook.com>
2021-06-07 21:27:01 -0300

    mysql

--- src/main/java/com/eventosapp/ListaToEventApplication.java
+++ src/main/java/com/eventosapp/ListaToEventApplication.java
@@ -1,4 +1,4 @@
-package com.eventosapp.controllers;
+package com.eventosapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
