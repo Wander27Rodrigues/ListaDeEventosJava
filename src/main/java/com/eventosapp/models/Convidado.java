5973ae52d1bd35301adfca0d31ed185cfbe7fbd3
wander rodrigues <wanderrodrigues_@outlook.com>
2021-08-24 23:26:42 -0300

    requerid

--- src/main/java/com/eventosapp/models/Convidado.java
+++ src/main/java/com/eventosapp/models/Convidado.java
@@ -3,12 +3,16 @@ package com.eventosapp.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Convidado {
 	
 	@Id
+	@NotEmpty
 	private String rg;
+	
+	@NotEmpty
 	private String nomeconvidado;
 	
 	@ManyToOne
